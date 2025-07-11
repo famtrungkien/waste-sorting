b5824eed1c510a34fc8cfecc6a4a745b668fa226
famtrungkien <phamtrungkien999@gmail.com>
2025-07-11 16:31:04 +0700

    Update effectlite.py

--- prediction/effectlite.py
+++ prediction/effectlite.py
@@ -15,7 +15,7 @@ from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning) 
 
-model = load_model('models/effectlite_model.h5')
+model = load_model('models/efficientnet_model.h5')
 
 
 def predict_fun(img_path):
